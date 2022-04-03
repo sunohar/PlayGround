e555c4a4835e50419d425f3e29965aff33dc46fd
sunohar <sunohar@yahoo.co.in>
2022-04-03 11:10:31 +0530

    loguru test

---
+++ test_loguru/test_loguru.py
@@ -0,0 +1,17 @@
+from loguru import logger
+
+logger.debug("That's it, beautiful and simple logging!")
+logger.info("That's it, beautiful and simple logging!")
+logger.warning("That's it, beautiful and simple logging!")
+logger.error("That's it, beautiful and simple logging!")
+logger.critical("That's it, beautiful and simple logging!")
+logger.exception("That's it, beautiful and simple logging!")
+
+Log = logger
+
+Log.debug("That's it, beautiful and simple logging! ~~~")
+Log.info("That's it, beautiful and simple logging! ~~~")
+Log.warning("That's it, beautiful and simple logging! ~~~")
+Log.error("That's it, beautiful and simple logging! ~~~")
+Log.critical("That's it, beautiful and simple logging! ~~~")
+Log.exception("That's it, beautiful and simple logging! ~~~")
